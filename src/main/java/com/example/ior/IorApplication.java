304e903a49acdc8d4e00aeb23398afca8a3a05c0
Буянова Татьяна Сергеевна <21458574@sigma.sbrf.ru>
2023-11-09 10:15:04 +0300

    First commit

---
+++ src/main/java/com/example/ior/IorApplication.java
@@ -0,0 +1,13 @@
+package com.example.ior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ior/IorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
